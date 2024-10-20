dba02b3ebae3c9d2fd4dbdc0c27e7ca4df40e188
Henok Samson <henoksamson57@gmail.com>
2024-10-21 02:18:37 +0300

    add: task 0

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,2 +1,17 @@
 #!/usr/bin/env python3
+""" Minmum opration"""
 
+
+def minOperations(n: int) -> int:
+    """Perform min operation"""
+    if n <= 1:
+        return 0
+    total_oprations = 0
+    divisor = 2
+
+    while n > 1:
+        while n % divisor == 0:
+            n /= divisor
+            total_oprations += divisor
+        divisor += 1
+    return total_oprations
